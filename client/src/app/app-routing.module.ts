bd7c5108a506cd704a17467d30705c5c883a1d11
Usama <muaslam96@gmail.com>
2024-10-14 15:39:46 +0500

    End of SSL configurations and angular setup and installation

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  // You can define your routes here
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
